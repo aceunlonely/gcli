6652b329d2f20c71c1fdca0d947e5a2f56ec8af6
apporoad <apporoadina@163.com>
2017-03-14 07:49:46 +0800

    edit

---
+++ src/test/demo003_main/03_testreadLine.js
@@ -0,0 +1,33 @@
+const readline = require('readline');
+
+function completer(line) {
+  const completions = '.help .error .exit .quit .q'.split(' ');
+  const hits = completions.filter((c) => { return c.indexOf(line) === 0 });
+  // show all completions if none found
+  return [hits.length ? hits : completions, line];
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    completer: completer,
+    historySize :50,
+});
+rl.setPrompt('gcli> ')
+rl.prompt();
+
+
+rl.on('line', (line) => {
+  switch(line.trim()) {
+    case 'hello':
+      console.log('world!');
+      break;
+    default:
+      console.log(`Say what? I might have heard '${line.trim()}'`);
+      break;
+  }
+  rl.prompt();
+}).on('close', () => {
+  console.log('Have a great day!');
+  process.exit(0);
+});
